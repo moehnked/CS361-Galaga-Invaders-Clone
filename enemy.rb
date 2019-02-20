67331e8a2b2c9a9db076bab58f9c372a4ab75f96
mwinb <mwinberry0101@gmail.com>
2019-02-20 11:21:31 -0800

    STUB out enemey movement

--- enemy.rb
+++ enemy.rb
@@ -1,10 +1,29 @@
 class Enemy
-  
+
+  BASIC_VELOCITY = 10
+  attr_accessor :x, :y, :width, :height;
   def initialize(x, y, width, height)
     @x = x
     @y = y
     @width =  width
     @height = height
   end
+
+  def move_down
+    self.y += BASIC_VELOCITY
+  end
+
+  def move_up
+    self.y -= BASIC_VELOCITY
+  end
+
+  def move_left
+    self.x -= BASIC_VELOCITY
+  end
+
+  def move_right
+    self.x += BASIC_VELOCITY
+  end
+
 
 end
