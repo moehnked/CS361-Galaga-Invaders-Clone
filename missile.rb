2805b2f5a53449d659ced68b4ec5adaef86c6cbd
Myles Moylan <moylanm@oregonstate.edu>
2019-03-06 11:37:21 -0800

    fix spelling and formatting

--- missile.rb
+++ missile.rb
@@ -1,18 +1,14 @@
 class Missile
-	attr_reader :x, :y, :velocity
-	
-	def initialize(x:, y:, velocity:)
-		@x = x
-		@y = y
-		@velocity = velocity
-	end
+  attr_reader :x, :y, :velocity
 
-	def move
-		y += velocity
-	end
+  def initialize(x:, y:, velocity:)
+    @x = x
+    @y = y
+    @velocity = velocity
+  end
 
-	def draw
-		# ...
-	end
-
+  def move
+    y += velocity
+  end
 end
+
