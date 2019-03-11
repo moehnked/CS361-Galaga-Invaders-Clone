82904913ae2aa2690f6767bec5564de25163385f
Otis0620 <Otis0620.github@gmail.com>
2019-03-10 21:15:50 -0700

    fix merge conflict

--- missile.rb
+++ missile.rb
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-class Missile includes Moveable
-	attr_reader :x, :y, :velocity
-	
-	def initialize(x:, y:, velocity:)
-		@x = x
-		@y = y
-		@velocity = velocity
-	end
-
-	def move
-		Moveable.move(self.y,velocity)
-	end
-	def draw
-		# ...
-	end
-=======
 class Missile
   attr_reader :x, :y, :velocity
 
@@ -23,7 +6,6 @@ class Missile
     @y = y
     @velocity = velocity
   end
->>>>>>> 1eb0435e54ec6e3b0e76f9dc474573d4c917f224
 
   def move
     y += velocity
